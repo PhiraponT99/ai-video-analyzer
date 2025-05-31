56bc4848a11696cd9894773d34305e8ad5391b15
PhiraponTH <119322480+PhiraponT99@users.noreply.github.com>
2025-05-31 23:28:31 +0700

    Add request handler for server-side rendering with React Router

--- api/index.js
+++ api/index.js
@@ -280,3 +280,7 @@ export {
   routes,
   ssr
 };
+import { createRequestHandler } from "@react-router/serve";
+import * as build from "../build/server/index.js";
+
+export default createRequestHandler({ build });
